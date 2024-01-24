bd9fd8255d50fc90aef2f58bddd5f5b71df9dc6f
SantiagoNavarroLlaguno <sanavllag@gmail.com>
2024-01-24 00:17:57 -0600

    Add print maze, fix maze, fix colors

--- path_finder.py
+++ path_finder.py
@@ -12,16 +12,25 @@ maze = [
     ["#", " ", "#", " ", "#", " ", "#", " ", "#"],
     ["#", " ", "#", " ", "#", " ", "#", " ", "#"],
     ["#", " ", " ", " ", " ", " ", " ", " ", "#"],
-    ["#", "#", "#", "#", "#", "#", "#", "#", "#"],
+    ["#", "#", "#", "#", "#", "#", "#", "X", "#"],
 ]
 
 
+def print_maze(maze, stdscr, path=[]):
+    BLUE = curses.color_pair(1)
+    RED = curses.color_pair(2)
+
+    for i, row in enumerate(maze):
+        for j, val in enumerate(row):
+            stdscr.addstr(i, j*2, val, BLUE)
+
+
 def main(stdscr):
-    curses.init_pair(1, curses.COLOR_BLUE, curses.COLOR_WHITE)
-    blue_and_black = curses.color_pair(1)
+    curses.init_pair(1, curses.COLOR_BLUE, curses.COLOR_BLACK)
+    curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
 
     stdscr.clear()
-    stdscr.addstr(5, 5, "Hello World!", blue_and_black)
+    print_maze(maze, stdscr)
     stdscr.refresh()
     stdscr.getch()
 
