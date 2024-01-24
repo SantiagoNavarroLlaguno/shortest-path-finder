f2725297d2c5257f664355bcfaf23311ac6c33e8
SantiagoNavarroLlaguno <sanavllag@gmail.com>
2024-01-24 00:06:51 -0600

    Add color to terminal

--- path_finder.py
+++ path_finder.py
@@ -17,8 +17,11 @@ maze = [
 
 
 def main(stdscr):
+    curses.init_pair(1, curses.COLOR_BLUE, curses.COLOR_WHITE)
+    blue_and_black = curses.color_pair(1)
+
     stdscr.clear()
-    stdscr.addstr(5, 5, "Hello World!")
+    stdscr.addstr(5, 5, "Hello World!", blue_and_black)
     stdscr.refresh()
     stdscr.getch()
 
